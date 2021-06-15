c8a41be1b3335a8400a79e09d89309b45ceb48e4
tangyunlong02 <tangyunlong02@meituan.com>
2021-06-15 19:53:27 +0800

    feat: 1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { add, minus, Text } from 'commons'
+import { add, minus, day, Text1 } from 'commons'
 console.log(add(1,3))
 console.log(minus(3,1))
-console.log(Text)
-// console.log(Text)
+console.log(Text1)
+console.log(day())
+
+// import Common from '../commons/utils/index.js'
+// console.log(Common)
+
+
 Vue.config.productionTip = false
 
 new Vue({
