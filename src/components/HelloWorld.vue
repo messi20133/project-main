d084fdbaf4184c0db6d4b9489494eff122be7d39
tangyunlong02 <tangyunlong02@meituan.com>
2021-06-15 10:21:57 +0800

    feat: save

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <Text1 msg="1111"></Text1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,8 +32,12 @@
 </template>
 
 <script>
+import {Text1} from 'commons'
 export default {
   name: 'HelloWorld',
+  components: {
+    Text1
+  },
   props: {
     msg: String
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { add, minus, Text } from 'commons'
+console.log(add(1,3))
+console.log(minus(3,1))
+console.log(Text)
+// console.log(Text)
 Vue.config.productionTip = false
 
 new Vue({
